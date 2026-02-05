f0d9669e2318ebed130dd9431d7bcc2b1ffad6c3
unknown <estherakinlade04@gmail.com>
2026-02-05 14:09:59 +0000

    dashboard changed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Dashboard</title>
+</head>
+<body>
+
 <h1>Dashboard</h1>
 
-<p>Welcome, {{ auth()->user()->name }}!</p>
-<p>Email: {{ auth()->user()->email }}</p>
+<p>Welcome, {{ $user->name }}!</p>
+<p>Email: {{ $user->email }}</p>
+
+<hr>
+
+<nav>
+    <ul>
+        <li><a href="/dashboard">Dashboard</a></li>
+        <li><a href="#">My Membership</a></li>
+        <li><a href="#">Classes</a></li>
+        <li><a href="#">Bookings</a></li>
+    </ul>
+</nav>
+
+<hr>
+
+<p>This is your dashboard. Fitness features will be added here.</p>
 
 <form method="POST" action="/logout">
     @csrf
     <button type="submit">Logout</button>
 </form>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -16,9 +17,8 @@ Route::get('/login', [AuthController::class, 'showLogin']);
 Route::post('/login', [AuthController::class, 'login']);
 
 // Dashboard (protected)
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware('auth');
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->middleware('auth');
 
 // Logout
 Route::post('/logout', [AuthController::class, 'logout']);
