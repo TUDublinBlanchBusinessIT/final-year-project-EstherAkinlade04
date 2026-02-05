73af59411e87d9e4447815c59b649f5f60a5f4b5
unknown <estherakinlade04@gmail.com>
2026-02-05 13:52:58 +0000

    dashboard working

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,9 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Dashboard</title>
-</head>
-<body>
-
 <h1>Dashboard</h1>
 
 <p>Welcome, {{ auth()->user()->name }}!</p>
 <p>Email: {{ auth()->user()->email }}</p>
 
-<hr>
-
-<p>This is your dashboard.</p>
-
 <form method="POST" action="/logout">
     @csrf
     <button type="submit">Logout</button>
 </form>
-
-</body>
-</html>
