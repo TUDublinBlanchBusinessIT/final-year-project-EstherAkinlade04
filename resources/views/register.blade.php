e5f2d1908f92a352d0abb9e605066cec4435d8d2
unknown <estherakinlade04@gmail.com>
2026-02-05 13:50:14 +0000

    register working

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,11 +1,20 @@
 <h2>Register</h2>
 
+@if ($errors->any())
+    <p style="color:red">{{ $errors->first() }}</p>
+@endif
+
 <form method="POST" action="/register">
     @csrf
 
-    <input type="text" name="name" placeholder="Name"><br><br>
-    <input type="email" name="email" placeholder="Email"><br><br>
-    <input type="password" name="password" placeholder="Password"><br><br>
+    <input type="text" name="name" placeholder="Name" required><br><br>
+    <input type="email" name="email" placeholder="Email" required><br><br>
+    <input type="password" name="password" placeholder="Password (min 6 chars)" required><br><br>
 
     <button type="submit">Register</button>
 </form>
+
+<p>
+    Already have an account?
+    <a href="/login">Login</a>
+</p>
