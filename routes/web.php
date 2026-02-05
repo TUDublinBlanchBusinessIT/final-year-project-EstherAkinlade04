1b4baaa9a0d4fbf4377c92b358b51879808808c5
unknown <estherakinlade04@gmail.com>
2026-02-05 14:22:41 +0000

    new web page working

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,14 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 
+// Home page (smart redirect)
 Route::get('/', function () {
+    // If user is logged in, send them to dashboard
+    if (auth()->check()) {
+        return redirect('/dashboard');
+    }
+
+    // Otherwise, show public home page
     return view('welcome');
 });
 
