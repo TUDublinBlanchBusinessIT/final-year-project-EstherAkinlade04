97b588c14c07fe16f12d86a1ce512974b6b3ce82
unknown <estherakinlade04@gmail.com>
2026-02-06 17:23:51 +0000

    confirmation works

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -10,7 +10,7 @@
     <input type="text" name="name" placeholder="Name" required><br><br>
     <input type="email" name="email" placeholder="Email" required><br><br>
     <input type="password" name="password" placeholder="Password (min 6 chars)" required><br><br>
-
+    <input type="password" name="password_confirmation" placeholder="Confirm Password" required><br><br>
     <button type="submit">Register</button>
 </form>
 
