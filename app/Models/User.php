684e2c5de9cd72ed3b27fffeb728fa87220fd6fa
unknown <estherakinlade04@gmail.com>
2026-02-13 13:45:59 +0000

    updated users

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,4 +16,19 @@ class User extends Authenticatable
         'password',
         'remember_token',
     ];
+
+    // A user has many bookings
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    // A user belongs to many fitness classes through bookings
+    public function fitnessClasses()
+    {
+        return $this->belongsToMany(
+            FitnessClass::class,
+            'bookings'
+        );
+    }
 }
