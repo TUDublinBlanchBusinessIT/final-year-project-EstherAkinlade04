477bcc8e7ff4e791e068d0c5971166a9e38d1214
unknown <estherakinlade04@gmail.com>
2026-02-13 13:47:55 +0000

    updated booking

--- app/Models/Booking.php
+++ app/Models/Booking.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Booking extends Model
 {
-    //
+    protected $fillable = [
+        'user_id',
+        'fitness_class_id',
+    ];
+
+    // A booking belongs to a user
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    // A booking belongs to a fitness class
+    public function fitnessClass()
+    {
+        return $this->belongsTo(FitnessClass::class);
+    }
 }
