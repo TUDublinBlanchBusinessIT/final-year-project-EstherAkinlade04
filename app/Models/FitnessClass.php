62fb11fbe68f78ac51bfbe1c4590ea115f999740
unknown <estherakinlade04@gmail.com>
2026-02-13 13:46:59 +0000

    updated fitness class

--- app/Models/FitnessClass.php
+++ app/Models/FitnessClass.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class FitnessClass extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+        'description',
+        'class_time',
+        'capacity',
+    ];
+
+    // A class has many bookings
+    public function bookings()
+    {
+        return $this->hasMany(Booking::class);
+    }
+
+    // A class has many users through bookings
+    public function users()
+    {
+        return $this->belongsToMany(
+            User::class,
+            'bookings'
+        );
+    }
 }
