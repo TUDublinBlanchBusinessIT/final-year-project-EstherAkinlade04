3d966c55e14795c39b19fdd60a606c95ab429f5f
unknown <estherakinlade04@gmail.com>
2026-03-09 11:28:38 +0000

    updated expiry reminders

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,23 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
+
+/*
+|--------------------------------------------------------------------------
+| Artisan Console Commands
+|--------------------------------------------------------------------------
+*/
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+/*
+|--------------------------------------------------------------------------
+| Scheduled Commands
+|--------------------------------------------------------------------------
+*/
+
+// Send membership expiry reminders every day
+Schedule::command('memberships:reminders')->daily();
